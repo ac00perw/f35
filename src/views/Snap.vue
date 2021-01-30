debc7d7e4bc583d8d9112297bb2d081eb8222019
Adam <a@acdubs.com>
2021-01-30 17:04:39 -0500

    adds snap people dot test

---
+++ src/views/Snap.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="people flex flex-wrap">
+    <template class="" v-for="i in people">
+         <span>&#149;</span>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+    name: "Snap",
+    data() {
+        return  {
+            people: 6800
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+.people span{
+    height: 10px;
+}
+</style>
